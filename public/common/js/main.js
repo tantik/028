bddb2fad9c6177d93f6d57f6aadb7fe1ca2d7091
Dim And <dimand81179@gmail.com>
2017-06-19 10:53:29 +0300

    Information page

--- public/common/js/main.js
+++ public/common/js/main.js
@@ -1,12 +1,12 @@
 $(window).resize(function() {
-
+	positionPopup();
 });
 
 $(document).ready(initPage);
 function initPage() {
 	mobileMenu();
 	scrollTo()
-
+	initPopup();
 }
 
 
@@ -27,4 +27,55 @@ function scrollTo() {
 		}
 		return false;
 	});
+}
+
+
+/* Popup initiation */
+function initPopup() {
+	$('a[data-popup]').click(function(e) {
+		$('.popup').removeClass('visible');
+		e.preventDefault();
+		var id = $(this).attr('data-popup');
+		var maskHeight = $(document).height();
+		$('.fader').css({'height':maskHeight});
+		$('.fader').addClass('visible');
+		$('#' + id).addClass('visible');
+		positionPopup();
+		if($('#' + id).height() >= $(window).height()){
+			$('#' + id).css({
+				top: $(window).scrollTop(),
+			});
+		} else {
+			$('#' + id).css({
+				top: $(window).scrollTop()+ $(window).height()/2,
+				marginTop: -$('#' + id).height()/2
+			});
+		}
+
+	});
+
+	$('.popup-close').click(function (e) {
+		e.preventDefault();
+		$('.fader').removeClass('visible');
+		$('.popup').removeClass('visible');
+	});
+	$('.fader').click(function () {
+		$(this).removeClass('visible');
+		$('.popup').removeClass('visible');
+	});
+}
+/* Popup position */
+function positionPopup(){
+	if($('.popup.visible').width() < $(document).width()){
+		$('.popup.visible').css({
+			'marginLeft': -($('.popup.visible').width())/2,
+			'left': '50%'
+		});
+	}
+	else{
+		$('.popup.visible').css({
+			'marginLeft': 0,
+			'left': 0
+		});
+	}
 }
